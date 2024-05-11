a15aa6bdf2083bf971f81838dc032afba30060f9
Firdevs Çolak <firdevscolak81@gmail.com>
2024-05-12 00:25:21 +0300

    EMİR UI ALERT!!!!!!

---
+++ Assets/audiomanager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAudioManager : MonoBehaviour
+{
+    [Header("----------Audio Source----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("----------Audio Clip----------")]
+    public AudioClip background;
+ 
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
+
